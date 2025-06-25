733107787a88653b9f821640bfa7866384019e24
zack-das <izackdas@gmail.com>
2025-06-25 11:54:11 +0300

    index.js Update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(driver => 
+    driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+     const drivers = [];
+  return drivers.filter(driver => 
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => 
+    driver.name.toLowerCase() === name.toLowerCase()
+  );
+}
